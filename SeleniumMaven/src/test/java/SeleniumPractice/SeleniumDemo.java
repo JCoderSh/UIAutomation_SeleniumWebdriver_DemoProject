c9611f3a30c0b24518c8b25001804894cce12936
JCoderSh <133141263+JCoderSh@users.noreply.github.com>
2023-06-07 14:05:39 -0500

    Update SeleniumDemo.java

--- SeleniumMaven/src/test/java/SeleniumPractice/SeleniumDemo.java
+++ SeleniumMaven/src/test/java/SeleniumPractice/SeleniumDemo.java
@@ -1,22 +1,27 @@
 package SeleniumPractice;
 
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
+import java.util.List;
 
-public class SeleniumDemo {
+public class GooglePageSearchFunc {
     public static void main(String[] args) {
 
 
-        // 44Set the browser driver directory path at system level then we do not need to provide below line of code
-        // System.setProperty("webdrivedr.chrome.driver", "C:\\Users\\15155\\chromedriver.exe");
+        // for Set the browser driver directory path at system level then we do not need to provide below line of code
+        // System.setProperty("webdrivedr.chrome.driver", "path");
 
-        String baseURL= "http://www.facebook.com";
-       // initialize the webdriver
+
+        // Needed below wait for driver syncronization issue.
+        //driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS); // This is deprecated method with selenium 4 - Duration needed to be used
+
+        String baseURL = "http://www.facebook.com";
+        // initialize the webdriver
         WebDriver driver = new ChromeDriver();
         // Maximize Window
         driver.manage().window().maximize();
@@ -27,20 +32,18 @@ public class SeleniumDemo {
         // Navigate to url
         driver.get(baseURL);
         System.out.println(driver.getTitle());
-        List<WebElement> b= driver.findElements(By.tagName("a"))
+        List<WebElement> b = driver.findElements(By.tagName("a"));
+
         System.out.println(b);
+
         driver.getTitle();
         // Accept the pop up for notification
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("disable-notification");
+        // options.addArguments("disable-notification");
 
         driver.quit();
         driver.close();
     }
 }
 
-/* System.setProperty("Webdriver.chrome.driver", "C:\\chromedriver.exe");
-           WebDriver driver = new ChromeDriver();
-           Jyoti.get("https://www.google.com");1
 
-           List<WebElement> b= driver.findElements(By.tagName("a"));*/
