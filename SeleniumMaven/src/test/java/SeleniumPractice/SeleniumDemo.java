a42e3dbed82015416d9fdd3f12054e30c9acc9ac
JCoderSh <133141263+JCoderSh@users.noreply.github.com>
2023-06-07 14:48:51 -0500

    Update SeleniumDemo.java

--- SeleniumMaven/src/test/java/SeleniumPractice/SeleniumDemo.java
+++ SeleniumMaven/src/test/java/SeleniumPractice/SeleniumDemo.java
@@ -13,36 +13,38 @@ public class GooglePageSearchFunc {
     public static void main(String[] args) {
 
 
-        // for Set the browser driver directory path at system level then we do not need to provide below line of code
+         // Earlier we needed to Set the browser driver directory path at system level then we do not need to provide below line of code
         // System.setProperty("webdrivedr.chrome.driver", "path");
 
+        String baseURL = "http://www.google.com";
 
-        // Needed below wait for driver syncronization issue.
-        //driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS); // This is deprecated method with selenium 4 - Duration needed to be used
-
-        String baseURL = "http://www.facebook.com";
-        // initialize the webdriver
+        // initialize the driver
         WebDriver driver = new ChromeDriver();
+
         // Maximize Window
         driver.manage().window().maximize();
 
-        // Needed below wait for driver syncronization issue.
-        //driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS); // This is deprecated method with selenium 4 - Duration needed to be used
+        // Needed below wait for driver synchronization issue.
+        //driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS); // This is deprecated method with selenium 4
+
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        // Navigate to url
+
+        //Navigate to url
         driver.get(baseURL);
+
         System.out.println(driver.getTitle());
-        List<WebElement> b = driver.findElements(By.tagName("a"));
+        //Alert AlertPopup= driver.switchTo().alert();
+        driver.findElement(By.xpath("//*[@id='gb']/div/div[1]/div/div[1]/a")).click();
+        driver.findElement(By.xpath("/html/body/header/div/div/div/a[1]")).click();
+        System.out.println(driver.getTitle());
 
-        System.out.println(b);
-
-        driver.getTitle();
-        // Accept the pop up for notification
-        ChromeOptions options = new ChromeOptions();
+        // Below code is for Accept the pop up for notification
+         ChromeOptions options = new ChromeOptions();
         // options.addArguments("disable-notification");
 
         driver.quit();
         driver.close();
+    }
     }
 }
 
